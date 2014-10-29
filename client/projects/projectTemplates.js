295d75c9be3821526e7f3fb78590f41a6adefc45
Jacob Oakes <jacoboakes92@gmail.com>
2014-10-28 20:12:02 -0400

    updating existing projects works now but not with validation

--- client/projects/projectTemplates.js
+++ client/projects/projectTemplates.js
@@ -8,36 +8,62 @@ Template.activeProjects.helpers({
 });
 
 Template.projectInfo.events = {
-    'blur .charge_number .project_name': function(event){
+    'blur .charge_number, blur .project_name, blur .date': function(event){
         var row = event.currentTarget.parentNode.parentNode;
+        var chargeNumber =  $(row).find('#charge_number')[0].value;
+        var name = $(row).find('#project_name')[0].value;
+        var startDate = $(row).find('#start_date')[0].value;
+        var endDate = $(row).find('#end_date')[0].value;
+        var manager = $(row).find('#manager')[0].value;
         ChargeNumbers.update(
             {
                 "_id": this._id
             },
             {
-                "id" : row.children[0].children[0].value,
-                "name": row.children[1].children[0].value,
-                "start_date": row.children[2].children[0].value,
-                "end_date": row.children[3].children[0].value,
-                "manager": row.children[4].children[0].value
+                "id" : chargeNumber,
+                "name": name,
+                "start_date": startDate,
+                "end_date": endDate,
+                "manager": manager
             }
-        )
+        );
+    },
+    'blur .manager-dropdown': function(event){
+        var row = event.currentTarget.parentNode.parentNode;
+        var manager = $(row).find('select')[0].value;
+        ChargeNumbers.update(
+            {
+                "_id": this._id
+            },
+            {
+                "id" : $(row).find('#charge_number')[0].value,
+                "name": $(row).find('#project_name')[0].value,
+                "start_date": $(row).find('#start_date')[0].value,
+                "end_date": $(row).find('#end_date')[0].value,
+                "manager": manager
+            }
+        );
+        var parent = event.currentTarget.parentNode;
+        parent.innerHTML = '<input type="text" class="manager" id="manager" value=' + manager + '>';
     },
     'click .manager': function(evt){
         var parent = evt.currentTarget.parentNode;
-        parent.innerHTML = '<select id="manager_to_change">' + $('#manager_to_add').html() + "</select>";
+        parent.innerHTML = '<select class="manager-dropdown" id="manager_to_change">' + $('#manager_to_add').html() + "</select>";
     }
 };
+
+Template.projectInfo.rendered = function(){
+    $.each($('[id=start_date]'), function(index, value){
+        $(value).datepicker({});
+    });
+    $.each($('[id=end_date]'), function(index, value){
+        $(value).datepicker({});
+    });
+}
 
 Template.addProject.rendered = function(){
     $('#start_date_to_add').datepicker({});
     $('#end_date_to_add').datepicker({});
-    $.each($('[id=start_date]'), function(index, value){
-        value.datepicker({});
-    });
-    $.each($('[id=end_date]'), function(index, value){
-        value.datepicker({});
-    });
 }
 
 Template.addProject.events = {
