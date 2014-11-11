0d4c6f918fb880ef57efb417e1194930ad0b6d01
Jacob Oakes <jacoboakes92@gmail.com>
2014-11-11 18:32:54 -0500

    setup the ldap call to either return the user or null

--- client/accounts_ldap_client.js
+++ client/accounts_ldap_client.js
@@ -1,12 +1,12 @@
 // borrowed from https://raw.githubusercontent.com/gui81/muster/master/client/lib/accounts_ldap_client.js
 
 authenticateLdapEmployee = function(username, password) {
-    Meteor.call('authenticateLdapEmployee', username, password, function(err, result){
+    Meteor.call('authenticateLdapEmployee', username, password, function(err, user){
         if(err){
             console.log("authentification error");
             // error handling
         } else {
-            console.log("authentifated = " + result);
+            console.log(user);
             // update meteor.users if true and set current user to them
         }
     });
--- server/accounts.js
+++ server/accounts.js
@@ -12,13 +12,38 @@ LDAP.client = ldapjs.createClient({
     url: Meteor.settings.ldap_url + Meteor.settings.ldap_search_base
 });
 
-LDAP.bind = function() {
-    var binddn = "uid=" + Meteor.settings.ldap_admin_account + "," + Meteor.settings.ldap_search_base;
+LDAP.search = function(username) {
+    var opts = {
+        filter: '(&(uid=' + username + ')(objectClass=posixAccount))',
+        scope: 'sub',
+        attributes: ['cn']
+    };
     var fut = new Future();
 
-    LDAP.client.bind(binddn, Meteor.settings.ldap_admin_password, function(err) {
+    LDAP.client.search(Meteor.settings.ldap_search_base, opts, function(err, search) {
         if (err) {
-            console.log('error = ' + err);
+            fut.return(null);
+        } else {
+            search.on('searchEntry', function(entry) {
+                fut.return(entry.object);
+            });
+
+            search.on('error', function(err) {
+                throw new Meteor.Error(500, 'LDAP server error');
+                fut.return(null);
+            });
+        }
+    });
+
+    return fut.wait();
+};
+
+LDAP.checkAccount = function(username, password) {
+    var binddn = "uid=" + username + "," + Meteor.settings.ldap_search_base;
+    var fut = new Future();
+
+    LDAP.client.bind(binddn, password, function(err) {
+        if (err) {
             fut.return(false);
         } else {
             fut.return(true);
@@ -28,107 +53,16 @@ LDAP.bind = function() {
     return fut.wait();
 }
 
-LDAP.search = function(username, callback) {
-    var opts = {
-        filter: '(&(uid=' + username + ')(objectClass=posixAccount))',
-        scope: 'sub',
-        attributes: ['cn', 'mail']
-    };
-    var fut = new Future();
-
-    LDAP.client.search(Meteor.settings.ldap_search_base, opts, function(err, search) {
-        if (err) {
-            console.log(err);
-            if (callback && typeof callback === "function") {
-                callback(false);
-            }
-            fut.return(false);
-        } else {
-            search.on('searchEntry', function(entry) {
-                var user = entry.object;
-                if (callback && typeof callback === "function") {
-                    callback(null, {cn: user.cn, mail: user.mail});
-                }
-                fut.return(true);
-            });
-
-            search.on('error', function(err) {
-                throw new Meteor.Error(500, 'LDAP server error');
-                fut.return(false);
-            });
-        }
-    });
-
-    return fut.wait();
-}
-
-LDAP.checkAccount = function(username, callback) {
-    var res = LDAP.bind();
-    if (!res) {
-        return false;
-    }
-    if (LDAP.search(username, callback)) {
-        return true;
-    } else {
-        return false;
-    }
-};
-
 // use Meteor.startup whenever you need something to load after all of the
 // other js files are loaded
 Meteor.startup(function() {
     Meteor.methods({
-        //getLdapEmployeeInfo: function(username) {
-        //    console.log("get user " + username);
-        //    var user_cn = null;
-        //    var user_mail = null;
-        //    var success = false;
-        //    try {
-        //        if (LDAP.checkAccount(username, function(error, result) {
-        //                user_cn = result.cn;
-        //                user_mail = result.mail;
-        //                console.log(result);
-        //                console.log(error);
-        //            })) {
-        //            success = true;
-        //        } else {
-        //            console.log("failed to get account");
-        //            success = false;
-        //        }
-        //    } catch(e) {
-        //        console.log("error");
-        //        success = false;
-        //    }
-        //    if (success) {
-        //        console.log(user_cn);
-        //        console.log(user_mail);
-        //        //EmployeeInfo.upsert(
-        //        //    {username: username},
-        //        //    {username: username,
-        //        //        cn: user_cn,
-        //        //        mail: user_mail
-        //        //    }
-        //        //);
-        //        //console.log(EmployeeInfo.find({}).fetch());
-        //        return true;
-        //    } else {
-        //        return false;
-        //    }
-        //},
         authenticateLdapEmployee : function(username, password){
-            var binddn = "uid=" + username + "," + Meteor.settings.ldap_search_base;
-            var fut = new Future();
-
-            LDAP.client.bind(binddn, password, function(err) {
-                if (err) {
-                    console.log('error = ' + err);
-                    fut.return(false);
-                } else {
-                    fut.return(true);
-                }
-            });
-
-            return fut.wait();
+            if(LDAP.checkAccount(username, password)){
+                return LDAP.search(username);
+            } else {
+                return null;
+            }
         }
     });
 });
