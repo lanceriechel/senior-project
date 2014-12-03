4f15b1e50e6e5d3b0ef4b5b93f450a2d39e20895
Teddy <sternetj@rose-hulman.edu>
2014-12-03 01:17:30 -0500

    Create time sheet for newly created user

--- server/accounts.js
+++ server/accounts.js
@@ -6,5 +6,26 @@ Accounts.onCreateUser(function (options, user) {
     if (options.profile) {
         user.profile = options.profile;
     }
+
+    var d = new Date(),
+        d2 = new Date();
+    d.setDate((d.getDate() - (d.getDay() + 6) % 7) - 1);
+    d2.setDate((d2.getDate() - (d2.getDay() + 6) % 7) + 6);
+    var dStr = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear(),
+        d2Str = (d2.getMonth() + 1) + "/" + d2.getDate() + "/" + d2.getFullYear();
+    TimeSheet.insert(
+        {
+            'startDate': dStr,
+            'endDate': d2Str,
+            'userId': user['_id'],
+            'active': 1,
+            'revision': [],
+            'projectEntriesArray': [],
+            'type' : 1,
+            'generalComment': '',
+            'concerns': '',
+            'submitted': false
+        }
+    );
     return user;
 });
