35fb5d616207fb2080a24f76ad08b8ad710bf20e
Teddy Sterne <sternetj@rose-hulman.edu>
2014-12-10 15:16:24 -0500

    Changed the time string to fix issue.
    Made it so new time sheets will be created for every user at 12:00 am Sunday morning instead of 12:00 am Monday Morning.

--- server/methods.js
+++ server/methods.js
@@ -39,7 +39,7 @@ Meteor.startup(function () {
         name: 'setup weekly timesheets',
         schedule: function(parser) {
             // parser is a later.parse object
-            return parser.text('at 00:00 on Sunday');
+            return parser.text('at 00:00 on Saturday');
         },
         job: function() {
             var d = new Date(),
