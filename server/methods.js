721a3df965f95647c76b87dc6266ce5b9c60c6ab
Dan Iverson <iversoda@dhcp-137-112-96-169.rose-hulman.edu>
2015-01-20 19:48:42 -0500

    Weekly status report v0.1

--- server/methods.js
+++ server/methods.js
@@ -1,8 +1,21 @@
+if (!String.prototype.format) {
+      String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number) { 
+          return typeof args[number] != 'undefined'
+            ? args[number]
+            : match
+          ;
+        });
+      };
+    }
 Meteor.startup(function () {
 
     process.env.MAIL_URL = 'smtp://noreply.scientiallc.timesheet%40gmail.com:N1esZd02FBi06WW@smtp.gmail.com:587/';
 
+    // First, checks if it isn't implemented yet.
     
+
     function setupMissingTimesheets() {
         /*
             Adds any missing timesheets for the current week
@@ -148,29 +161,79 @@ Meteor.startup(function () {
             console.log(job.details.schedule_text);
             switch (job.type) {
                 case 'email':
-                    SyncedCron.add({
-                        name: job.type + 'job: ' + job._id,
-                        schedule: function (parser) {
-                            // parser is a later.parse object
-                            return parser.text(job.details.schedule_text);
-                        },
-                        job: function () {
-                            var d = new Date();
-                            d.setDate((d.getDate() - (d.getDay() + 6) % 7) - 1);
-                            var dStr = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
+                    switch (job.details.type) {
+                        case 'reminder':
+                            SyncedCron.add({
+                                name: job.type + 'job: ' + job._id,
+                                schedule: function (parser) {
+                                    // parser is a later.parse object
+                                    return parser.text(job.details.schedule_text);
+                                },
+                                job: function () {
+                                    var d = new Date();
+                                    d.setDate((d.getDate() - (d.getDay() + 6) % 7) - 1);
+                                    var dStr = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
 
-                            var toRemind = [];
-                            TimeSheet.find({startDate: dStr, submitted: false}).forEach(function (sheet) {
-                                toRemind.push({_id: sheet.userId});
-                            });
+                                    var toRemind = [];
+                                    TimeSheet.find({startDate: dStr, submitted: false}).forEach(function (sheet) {
+                                        toRemind.push({_id: sheet.userId});
+                                    });
 
-                            toSendEmail = [];
-                            Meteor.users.find({$or: toRemind}).map(function (u) {
-                                toSendEmail.push(u.email);
+                                    toSendEmail = [];
+                                    Meteor.users.find({$or: toRemind}).map(function (u) {
+                                        toSendEmail.push(u.email);
+                                    });
+                                    Meteor.call('sendEmail', toSendEmail, 'Please Submit Your Timesheet', EmailTemplates.getReminderEmail());
+                                }
                             });
-                            Meteor.call('sendEmail', toSendEmail, 'Please Submit Your Timesheet', EmailTemplates.getReminderEmail());
-                        }
-                    });
+                        case 'report' :
+
+                           SyncedCron.add({
+                                name: job.type + 'job: ' + job._id,
+                                schedule: function (parser) {
+                                    // parser is a later.parse object
+                                    return parser.text(job.details.schedule_text);
+                                },
+                                job: function () {
+                                    var d = new Date();
+                                    var d2 = new Date();
+                                    d.setDate((d.getDate() - (d.getDay() + 6) % 7) - 1);
+                                    var dStr = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
+                                    d2.setDate((d2.getDate() - (d2.getDay() + 6) % 7) +6);
+                                    var dStr2 = (d2.getMonth() + 1) + '/' + d2.getDate() + '/' + d2.getFullYear();
+                                    var start= dStr;
+                                    var end=dStr2;
+                                    var projectArray = {};//new Array();
+                                    var projectHours = {};
+                                    ChargeNumbers.find().forEach(function (project){
+                                        //console.log(project.name);
+                                        projectArray[project.id]= project.name;
+                                        projectHours[project.id]=0;
+                                    });
+                                    TimeSheet.find({startDate: dStr}).forEach(function (sheet){
+                                        for (var pIndex in sheet.projectEntriesArray) {
+                                            var currentProject = sheet.projectEntriesArray[pIndex].projectID;
+                                            for (var eIndex in sheet.projectEntriesArray[pIndex].EntryArray) {
+                                                var entry= sheet.projectEntriesArray[pIndex].EntryArray[eIndex];
+                                                var sum =0;
+                                                for (var hours in entry.hours){
+                                                    sum+=entry.hours[hours];
+                                                }
+                                                projectHours[currentProject]+=sum;
+                                            }
+                                        }
+
+                                    });
+                                    var report = {};
+                                    for (var key in projectHours){
+                                        report[projectArray[key]]= projectHours[key];
+                                    }
+                                    //console.log(report);
+                                    Meteor.call('sendEmail', 'iversoda@rose-hulman.edu', 'Projects', EmailTemplates.getReportEmail(report,'Comments',start, end));
+                                }
+                                   
+                            });
+                    }
                     break;
             }
             SyncedCron.start();
